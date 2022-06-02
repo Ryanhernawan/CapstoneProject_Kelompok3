408ddc8ff7a90e58ba649713774755565e8a88e1
Ryanhernawan <ryan.hernawan1508@gmail.com>
2022-06-02 15:37:09 +0700

    test

--- src/main/java/com/capstone/kelompok3/repository/InstructorsRepositoy.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.kelompok3.repository;
-
-import com.capstone.kelompok3.entity.Instructors;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InstructorsRepositoy extends JpaRepository<Instructors, Long> {
-}
