4093ccefe5460faa100a2cd1c57d525d3973d6ec
Ryanhernawan <ryan.hernawan1508@gmail.com>
2022-06-08 09:59:08 +0700

    update project

--- src/main/java/com/alterra/capstone/entity/Class.java
+++ src/main/java/com/alterra/capstone/entity/Class.java
@@ -1,15 +1,18 @@
 package com.alterra.capstone.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@Data
 @Entity
 @Table(name = "class")
 public class Class {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_class", nullable = false)
-    private Integer id;
+    private Long id;
 
     @MapsId
     @OneToOne(fetch = FetchType.LAZY, optional = false)
@@ -54,116 +57,6 @@ public class Class {
     @Column(name = "updated_at")
     private LocalDate updatedAt;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Booking getBooking() {
-        return booking;
-    }
-
-    public void setBooking(Booking booking) {
-        this.booking = booking;
-    }
-
-    public User getIdUser() {
-        return idUser;
-    }
-
-    public void setIdUser(User idUser) {
-        this.idUser = idUser;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getStartAt() {
-        return startAt;
-    }
-
-    public void setStartAt(LocalDate startAt) {
-        this.startAt = startAt;
-    }
-
-    public LocalDate getEndAt() {
-        return endAt;
-    }
-
-    public void setEndAt(LocalDate endAt) {
-        this.endAt = endAt;
-    }
-
-    public Integer getQtyUsers() {
-        return qtyUsers;
-    }
-
-    public void setQtyUsers(Integer qtyUsers) {
-        this.qtyUsers = qtyUsers;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Instructor getIdInstructor() {
-        return idInstructor;
-    }
-
-    public void setIdInstructor(Instructor idInstructor) {
-        this.idInstructor = idInstructor;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public LocalDate getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDate getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 
 }
