03ca916d66c33e0d74a97716069e7ce246fd271b
RahmatSetia <rahmasetiq@gmail.com>
2022-06-29 16:37:45 +0700

    implementation cors origin

--- src/main/java/com/alterra/capstone/CapstoneProjectApplication.java
+++ src/main/java/com/alterra/capstone/CapstoneProjectApplication.java
@@ -2,9 +2,21 @@ package com.alterra.capstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CapstoneProjectApplication {
+	@Bean
+	public WebMvcConfigurer getCorsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true).allowedMethods("GET", "PUT", "POST", "DELETE");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneProjectApplication.class, args);
