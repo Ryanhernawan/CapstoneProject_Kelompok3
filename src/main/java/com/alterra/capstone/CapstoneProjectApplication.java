4747c4847510095163492e434df0df47b2499f90
RahmatSetia <rahmasetiq@gmail.com>
2022-07-08 21:30:05 +0700

    cors origin deploy FE

--- src/main/java/com/alterra/capstone/CapstoneProjectApplication.java
+++ src/main/java/com/alterra/capstone/CapstoneProjectApplication.java
@@ -14,10 +14,11 @@ public class CapstoneProjectApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry){
 				registry.addMapping("/**").
-						allowedOrigins("http://localhost:3000").
-						allowedOrigins("http://react-front-end-capstone-kel-3-git-fetchdata-zaidantr.vercel.app").
-						allowedOrigins("http://react-front-end-capstone-kel-3.vercel.app").
-						allowCredentials(false).
+						allowedOrigins("http://localhost:3000",
+								"http://react-front-end-capstone-kel-3-git-fetchdata-zaidantr.vercel.app",
+								"https://react-front-end-capstone-kel-3.vercel.app"
+								).
+						allowCredentials(true).
 						allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
 			}
 		};
